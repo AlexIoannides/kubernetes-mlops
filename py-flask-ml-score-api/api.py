d770f3f07746bf387c76d9792749467ae15758d1
alexioannides <alex.ioanndies@yahoo.co.uk>
2019-02-19 12:24:21 +0000

    Tidy api.py with better docstring and type hints

--- py-flask-ml-score-api/api.py
+++ py-flask-ml-score-api/api.py
@@ -6,26 +6,38 @@ This module defines a simple REST API for an imaginary Machine Learning
 (ML) model. It will be used for testing Docker and Kubernetes.
 """
 
-from flask import Flask, jsonify, make_response, request
+from typing import Iterable
+
+from flask import Flask, jsonify, make_response, request, Response
 
 app = Flask(__name__)
 
 
 @app.route('/score', methods=['POST'])
-def score():
+def score() -> Response:
     """Score data using an imaginary machine learning model.
 
-    This API expects a JSON payload that includes the following fields:
-        X - an array of input features,
+    This API endpoint expects a JSON payload with a field called `X`
+    containing an iterable sequence of features to send to the model.
+    This data is parsed into Python dict and made available via
+    `request.json`
 
-    And will return a JSON payload with the following fields:
-        score - an array of predictions.
+    If `X` cannot be found in the parsed JSON data, then an exception
+    will be raised. Otherwise, it will return a JSON payload with the
+    `score` field containing the model's prediction.
     """
+
     try:
         features = request.json['X']
-        return make_response(jsonify({'score': features}))
+        prediction = model_predict(features)
+        return make_response(jsonify({'score': prediction}))
     except KeyError:
         raise RuntimeError('"X" cannot be be found in JSON payload.')
+
+
+def model_predict(x: Iterable[float]) -> Iterable[float]:
+    """Dummy prediction function."""
+    return x
 
 
 if __name__ == '__main__':
